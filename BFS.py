66449a9b3122de823093adcc09739563516d75fc
Blake Johnson <blakeconnorjohnson@gmail.com>
2020-03-01 15:50:40 -0600

    pushing so we can debug on pycharm

--- BFS.py
+++ BFS.py
@@ -30,9 +30,11 @@ def BFS(xy1, xy2, gameStateData):
 
 # {(x,y): set((x1,y1),(x2,y2)...) , (x1,y1) :}
 def coinGrouping(xy, gameStateData):
+	food = gameStateData.getFood().asList()
+	capsules = gameStateData.getCapsules()
 	q = Queue()
 	q.push(xy)
-	seen = {xy: None}
+	seen = set()
 	coinGroups = {}
 	while not q.isEmpty():
 		nextPoint = q.pop()
@@ -40,13 +42,13 @@ def coinGrouping(xy, gameStateData):
 		for neighbor in neighbors:
 			if neighbor not in seen:
 				q.push(neighbor)
-				seen[neighbor] = nextPoint
-				
-				if gameStateData.getFood()[neighbor[0]][neighbor[1]]:
+				seen.add(neighbor)
+
+				if food[neighbor[0]][neighbor[1]] or neighbor in capsules:
 					# we have a coin at neighbor
 					coinGroups[neighbor] = coinGroup(neighbor, gameStateData)
 					for coin in coinGroups[neighbor]:
-						seen[coin] = neighbor
+						seen.add(coin)
 	return coinGroups
 
 
@@ -54,16 +56,20 @@ def coinGrouping(xy, gameStateData):
 	
 # given a coin location, adds all coins connected to it to a set.
 def coinGroup(xy, gameStateData):
+	food = gameStateData.getFood().asList()
+	capsules = gameStateData.getCapsules()
 	q = Queue()
 	q.push(xy)
-	seen = set((xy))
+	seen = set(xy)
+	seenCoin = set(xy)
 	while not q.isEmpty():
 		nextPoint = q.pop()
 		neighbors = game.Actions.getLegalNeighbors(nextPoint, gameStateData.getWalls())
 		for neighbor in neighbors:
 			if neighbor not in seen:
-				seen.add(nextPoint)
-				if gameStateData.getFood()[neighbor[0]][neighbor[1]] or gameStateData.getCapsules()[neighbor[0]][neighbor[1]]:
+				seen.add(neighbor)
+				if food[neighbor[0]][neighbor[1]] or neighbor in capsules:
+					seenCoin.add(neighbor)
 					q.push(neighbor)
 	return seen
 
